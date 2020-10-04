f6780a588ceab45bec11670b84719e9f30d0a8bf
Lucca Scarpa <lucca.scarpa@poli.ufrj.br>
2020-10-04 11:50:57 -0300

    Changes made due to the alteration of the app name

--- webserver/django/api/urls.py
+++ webserver/django/api/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from core.views import DataSetViewSet, ImageViewSet, ImageMetaDataViewSet
+from image_service.views import DataSetViewSet, ImageViewSet, ImageMetaDataViewSet
 from rest_framework import routers
 
 router = routers.DefaultRouter()
--- webserver/django/core/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import DataSet, Image, ImageMetaData
-# Register your models here.
-
-admin.site.register(DataSet)
-admin.site.register(Image)
-admin.site.register(ImageMetaData)

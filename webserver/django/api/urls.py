8e38a7c2819d010f4ba9b5a080d2f8df9d175338
Fernanda Sant <fernandasantana2304@gmail.com>
2020-10-14 11:36:27 -0300

    endpoints to acess data

--- webserver/django/api/urls.py
+++ webserver/django/api/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from image_service.views import DataSetViewSet, ImageViewSet, ImageMetaDataViewSet
+from image_service.views import DataSetViewSet, ImageViewSet, ImageMetaDataViewSet, DataBaseViewSet
 from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register(r'dataset', DataSetViewSet)
 router.register(r'image', ImageViewSet)
 router.register(r'image_meta_data', ImageMetaDataViewSet)
+router.register(r'database', DataBaseViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),

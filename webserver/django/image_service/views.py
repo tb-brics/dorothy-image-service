2672db388e30a46103e4f7262aaec1a10138c28b
Lucca Scarpa <lucca.scarpa@poli.ufrj.br>
2020-10-26 23:33:47 -0300

    Deleting old migrations and creating new loading methods

--- webserver/django/image_service/views.py
+++ webserver/django/image_service/views.py
@@ -3,14 +3,14 @@ from rest_framework import viewsets
 from .models import DataSet, Image, ImageMetaData
 from .serializers import DataSetSerializer, ImageSerializer, ImageMetaDataSerializer
 
-class DataSetViewSet(viewsets.ModelViewSet):
+class DataSetViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = DataSet.objects.all()
     serializer_class = DataSetSerializer
 
-class ImageViewSet(viewsets.ModelViewSet):
+class ImageViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Image.objects.all()
     serializer_class = ImageSerializer
 
-class ImageMetaDataViewSet(viewsets.ModelViewSet):
+class ImageMetaDataViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = ImageMetaData.objects.all()
     serializer_class = ImageMetaDataSerializer

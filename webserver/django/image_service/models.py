e3e5048424d05e327fe2a21dcf856ad58346653a
Fernando Ferreira <fernandogferreira@gmail.com>
2020-11-04 10:30:46 -0300

    :art: Models and serializers working

--- webserver/django/image_service/models.py
+++ webserver/django/image_service/models.py
@@ -1,25 +1,8 @@
-from django.core.files import File as FileWrapper
-
-def _handle_directory(self, directory_path, directory):
-    for root, subFolders, files in os.walk(directory_path):
-        for filename in files:
-            self.cnt_files += 1
-            new_file = File(
-                 directory=directory, filename=filename,
-                 file=os.path.join(root, filename),
-                 uploader=self.uploader)
-            with open(os.path.join(root, filename), 'r') as f:
-                file_wrapper = FileWrapper(f)
-                new_file = File(
-                    directory=directory, filename=filename,
-                    file=file_wrapper,
-                    uploader=self.uploader)
-                new_file.save()
-
 """Importing models module to create models classes"""
 from django.db import models
 from django.conf import settings
 import os
+
 
 class DataSet(models.Model):
     """Class for datasets"""
@@ -32,18 +15,15 @@ class DataSet(models.Model):
 
 class Image(models.Model):
     """Class for images"""
-    dataset = models.ForeignKey(DataSet, on_delete=models.CASCADE,)
-    image_path = models.URLField()
+    dataset = models.ForeignKey(DataSet, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='static/image_service/image/')
 
-    def __str__(self):
-        return str(self.dataset)
 
 class ImageMetaData(models.Model):
     """Class for the meta data"""
-    dataset = models.ForeignKey(DataSet, on_delete=models.CASCADE,)
-    image = models.ImageField(upload_to='static/image_service/image/')
+    image = models.OneToOneField(Image,
+                                 related_name='metadata',
+                                 primary_key=True,
+                                 on_delete=models.CASCADE)
     has_tb = models.BooleanField()
     original_report = models.CharField(max_length = 10000, null=True)
-
-    def __str__(self):
-        return str(self.image)

fce96226f3eea42c18092a77d130af9c5d4df843
Lucca Scarpa <lucca.scarpa@poli.ufrj.br>
2020-11-11 20:14:10 -0300

    Fixed images issues in load method

--- webserver/django/image_service/models.py
+++ webserver/django/image_service/models.py
@@ -1,14 +1,16 @@
 """Importing models module to create models classes"""
 from django.db import models
 from django.conf import settings
+from django.contrib.postgres.fields import ArrayField
 import os
 import uuid
+
 
 
 class DataSet(models.Model):
     """Class for datasets"""
     name = models.CharField(unique=True, max_length = 10000)
-    image_formats = models.CharField(max_length = 10000, default="")
+    image_formats = (models.CharField(max_length = 1000, default=""))
 
     def __str__(self):
         return str(self.name)
@@ -17,9 +19,9 @@ class DataSet(models.Model):
 class Image(models.Model):
     """Class for images"""
     dataset = models.ForeignKey(DataSet, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='static/image_service/image/')
+    image = models.ImageField()
 
-    
+
     def save(self, *args, **kwargs):
         dataset_name = self.dataset.name.tolower().replace('_','')
         image_filename = os.path.basename(self.image.name).tolower().replace('_','')
@@ -28,7 +30,7 @@ class Image(models.Model):
         super(Image, self).save(*args, **kwargs)
 
 
-   
+
 
 
 class ImageMetaData(models.Model):
@@ -38,4 +40,4 @@ class ImageMetaData(models.Model):
                                  primary_key=True,
                                  on_delete=models.CASCADE)
     has_tb = models.BooleanField()
-    original_report = models.CharField(max_length = 10000, null=True)
+    original_report = models.TextField(null=True)
--- webserver/django/image_service/views.py
+++ webserver/django/image_service/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import DataSet, Image, ImageMetaData
 from .serializers import DataSetSerializer, ImageSerializer, ImageMetaDataSerializer
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 class DataSetViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = DataSet.objects.all()
@@ -10,6 +11,9 @@ class DataSetViewSet(viewsets.ReadOnlyModelViewSet):
 class ImageViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Image.objects.all()
     serializer_class = ImageSerializer
+    filter_backends = (SearchFilter, OrderingFilter)
+    search_fields = (['dataset__name'])
+
 
 class ImageMetaDataViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = ImageMetaData.objects.all()

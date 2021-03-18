840e2fa010c6706b17e70c0d57f9efbce50cdc98
fernandastn <fernandasantana2304@gmail.com>
2021-03-17 23:23:50 -0300

    Serve Images as response

--- webserver/django/image_service/views.py
+++ webserver/django/image_service/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .models import DataSet, Image, ImageMetaData
-from .serializers import DataSetSerializer, ImageSerializer, ImageMetaDataSerializer
+from .models import DataSet, Image, ImageMetaData, Report, ImageSampling
+from .serializers import DataSetSerializer, ImageSerializer, ImageMetaDataSerializer, ReportSerializer, ImageSamplingSerializer
+from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.response import Response
+from django.views.generic.detail import DetailView
+
+
+
 
 class DataSetViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = DataSet.objects.all()
@@ -10,6 +16,14 @@ class DataSetViewSet(viewsets.ReadOnlyModelViewSet):
 class ImageViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Image.objects.all()
     serializer_class = ImageSerializer
+
+class ImageFileView(DetailView):
+    model = Image    
+    def get(self, request, *args, **kwargs):
+        image = self.image
+        image_data = image.read()
+        return HttpResponse(image_data, content_type="image/png")
+
 
 class ImageMetaDataViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = ImageMetaData.objects.all()

ea33bc09ca2e0f997223da53bc8e049a56719b4f
Fernando Ferreira <fernandogferreira@gmail.com>
2021-04-07 00:37:38 -0300

    Remove /media path.

--- webserver/django/api/urls.py
+++ webserver/django/api/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+]

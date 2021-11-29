ca5370e8962c3b0c0b1c05d223d2b5697dd2e7a6
Lucca Scarpa <lucca.scarpa@poli.ufrj.br>
2021-11-29 20:55:58 -0300

    Created new script for sampling, using json methods

--- webserver/django/image_service/management/commands/load_sampling.py
+++ webserver/django/image_service/management/commands/load_sampling.py
@@ -1,15 +1,13 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from image_service.models import Image, ImageSampling
-import os.path
-from optparse import make_option
 from datetime import date
-import ast
+import json
 
 
 class Command(BaseCommand):
 
     def add_arguments(self, parser):
-        parser.add_argument('txt_file_path', type=str, help='Insert the path for the file which contains the images dictionary')
+        parser.add_argument('json_file_path', type=str, help='Insert the path for the file which contains the images dictionary')
 
     def handle(self,*args,**options):
 
@@ -17,18 +15,19 @@ class Command(BaseCommand):
         for image in Image.objects.all():
             images_ids.append(image.project_id)
 
-        with open(options['txt_file_path'], 'r') as f:
-            contents = f.read()
-            images_to_add_dict = ast.literal_eval(contents)
+        f = open(options['json_file_path'],)
+
+        data = json.load(f)
+
         f.close()
 
-        for key in list(images_to_add_dict.keys()):
-            img_id = [s for s in images_ids if images_to_add_dict[key] in s][0]
+        for key in list(data.keys()):
+            img_id = [s for s in images_ids if data[key] in s][0]
             image_sampling__obj = ImageSampling(
                 image=Image.objects.filter(project_id=img_id)[0],
                 insertion_date=str(date.today()),
-                rank_position=key)
+                rank_position=int(key))
             image_sampling__obj.save()
 
-        print(f'Added {len(images_to_add_dict)} images to sampling')
+        print(f'Added {len(data)} images to sampling')
 
